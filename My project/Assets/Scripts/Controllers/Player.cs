36e33c2fc80dc786c10b1578b0f630444c614f16
Rye L <148375551+ryeLS@users.noreply.github.com>
2024-09-20 14:57:50 -0400

    added project

---
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+
+    private Vector3 velocity = Vector3.right * 0.001f;
+    void Update()
+    {
+        //transform.position += velocity;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            PlayerMovement();
+        }
+    }
+    void PlayerMovement()
+    {
+        transform.position += -velocity;
+    }
+
+}
