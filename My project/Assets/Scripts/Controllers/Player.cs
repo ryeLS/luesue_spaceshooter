ea9aff704031ade053d422378f404c244d9cce01
Rye L <148375551+ryeLS@users.noreply.github.com>
2024-09-20 17:03:16 -0400

    task1a done and task1b almost

--- My project/Assets/Scripts/Controllers/Player.cs
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -9,18 +9,50 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
-    private Vector3 velocity = Vector3.right * 0.001f;
+    public float maxspeed = 3f;
+    public float timetoreachspeed = 2f;
+
+    private Vector3 velocity = Vector3.zero;
+    public float acceleration = 1f;
+
+    private void Start()
+    {
+
+        acceleration = maxspeed / timetoreachspeed;
+ 
+    }
+
     void Update()
     {
-        //transform.position += velocity;
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            PlayerMovement();
-        }
+        PlayerMovement(); 
     }
     void PlayerMovement()
     {
-        transform.position += -velocity;
+       // velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+
+            velocity += Vector3.left * acceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity += Vector3.right * acceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity += Vector3.up * acceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity += Vector3.down * acceleration * Time.deltaTime;
+        }
+        //velocity = velocity.normalized * speed;
+         //fix this
+         //find magnitude (its in the sliders) for the velocity and have an if statement for if magnitude>maxspeed
+        velocity = velocity.normalized * maxspeed;
+        
+        
+        transform.position += velocity * Time.deltaTime;
+
     }
 
 }
